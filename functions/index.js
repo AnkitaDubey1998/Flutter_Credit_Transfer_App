e6be023a6c984e54c41482f9a43ce9132bc14fca
Ankita Kar <ankitakar98@gmail.com>
2020-05-28 02:21:33 +0530

    added firebase function for transfer credit

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,78 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
+
+
+// function for sending notification to receiver on receiving credit
+exports.TransferNotification = functions.firestore.document('transactions/{receiverUid}/{sameuid}/{transactionId}')
+.onCreate( async (snapshot, context) => {
+
+	var receiverUid = context.params.receiverUid;
+	var transactionId = context.params.transactionId;
+
+	var type = ''
+	var senderName = '';
+
+	await admin.firestore().doc('transactions/'+receiverUid+'/'+receiverUid+'/'+transactionId).get()
+	.then(doc => {
+		type = doc.data().type;
+		senderName = doc.data().name;
+		return true;
+	})
+	.catch(error => {
+		console.log("error: ", error);
+    	return false;
+	});
+
+	console.log('type: ', type);
+	console.log('sender name: ', senderName);
+
+	if(type == 'from') {
+		return await admin.firestore().doc('users/'+receiverUid).get()
+		.then(doc => {
+			var deviceTokens = doc.data().deviceTokens;
+			console.log("device tokens: ", deviceTokens);
+
+			var payload = {
+	    		notification: {
+	    			title: 'Received Credit',
+	    			body:  senderName + ' has sent ' + requestCredit + ' credits ',
+	    			sound: 'default'
+	    		}
+			};
+
+			return admin.messaging().sendToDevice(deviceTokens, payload)
+	    	.then(value => {
+	    		console.log("helooo");
+	    		return console.log("Notification sent");
+	    	})
+	    	.catch(error => {
+	    		return console.log('error: ', error);
+	    	});
+
+		}).catch(error => {
+			console.log("error: ", error);
+	    	return false;
+		});
+
+		} else {
+		console.log('this is "to" type transaction');
+		return console.log('not valid');
+	}
+    
+
+});
+
+
+
+
+// function for sending notification of requests
 exports.RequestNotification = functions.firestore.document('requests/{receiverUid}/from/{requestId}')
 .onCreate( async (snapshot, context) => {
 
     if(snapshot.empty) {
        console.log('No device');
     }
-
 
     var receiverUid = context.params.receiverUid;
     var requestId = context.params.requestId;
@@ -48,7 +113,6 @@ exports.RequestNotification = functions.firestore.document('requests/{receiverUi
     		}
 		};
 
-		
     	return admin.messaging().sendToDevice(deviceTokens, payload)
     	.then(value => {
     		console.log("helooo");
